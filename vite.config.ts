462390df2ad8a654e3931558d53fb58ff4f09e29
zero <u9648u6653u52c7@gmail.com>
2024-04-09 11:43:29 +0800

    chore: add configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: "lib/index.ts",
+      name: "CodeRain",
+      fileName: "index",
+    },
+  },
+});
